9cd0870e56fd74f11970f1727048e255e442a044
ghosty <nvrn24spf@proton.me>
2023-06-28 12:47:43 +0300

    fix plugins

--- home/cli/shell/zsh/zsh-plugins/lplugins.zsh
+++ home/cli/shell/zsh/zsh-plugins/lplugins.zsh
@@ -4,4 +4,4 @@ source $HOME/.config/zsh-plugins/fzf_completion.zsh
 source $HOME/.config/zsh-plugins/pomodoro.zsh
 source $HOME/.config/zsh-plugins/settings.zsh
 source $HOME/.config/zsh-plugins/binds.zsh
-source $HOME/.config/zsh-plugins/zeno.zsh/zeno.zsh
+source $HOME/.config/zsh-plugins/zeno.zsh
