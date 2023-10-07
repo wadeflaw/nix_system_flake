9d53418f104d090108699221896ff4320c77f93f
ghosty <nvrn24spf@proton.me>
2023-10-07 13:45:03 +0300

    upd: nod

--- hosts/nixOnDroid/ghost/default.nix
+++ hosts/nixOnDroid/ghost/default.nix
@@ -58,7 +58,6 @@
   home-manager.config = { pkgs, ... }: {
     home.stateVersion = "22.05";
     programs.ssh.enable = true;
-    services.sshd.enable = true;
     # imports = [
     #   # ../../home/misato/programs/shell
     #   # ../../home/misato/programs/neofetch
